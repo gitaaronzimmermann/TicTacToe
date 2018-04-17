3309a4310c631386b603ef6d2c5b6e81e2469599
Aaron Zimmermann <aaron.zimmermann98@googlemail.com>
2018-04-17 09:24:35 +0200

    Buttons
    Buttonscript
    Restart Button
    Exit Button
    Play Button

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+    // Start
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene("SceneTest");
+    }
+
+    // Restart
+    public void ButtonRestart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Exit
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+
+	
+}
